f7156d7f094a94048df4a7992f0c095caa352f64
Christopher Moje <42236736+chrismoje@users.noreply.github.com>
2023-03-07 19:36:24 +0800

    My Personal CV

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const form = document.forms.form;
+const btnSubmit = document.getElementById("btnSubmit");
+
+function fsubmit() {
+    alert("Online Form Submit is still a work in progress");
+
+};
+
+
